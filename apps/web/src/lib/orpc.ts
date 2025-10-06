8bfad138ab7e971de46d9b7ea49fb4be8c7769f9
François Lachèse <francois.lachese@gmail.com>
2025-10-06 22:34:08 +0200

    refactor: "custom" query client

--- apps/web/src/lib/orpc.ts
+++ apps/web/src/lib/orpc.ts
@@ -3,18 +3,28 @@ import type { Client } from "@cm3k/server";
 import { createORPCClient, onError } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      console.error(error);
+    },
+  }),
+});
 
 const link = new RPCLink({
+  url: "http://localhost:3000/rpc",
   headers: () => ({
     authorization: "Bearer token",
   }),
   // fetch: <-- provide fetch polyfill fetch if needed
-  interceptors: [
-    onError((error) => {
-      console.error(error);
-    }),
-  ],
-  url: "http://localhost:3000/rpc",
+  // interceptors: [
+  //   onError((error) => {
+  //     console.log("#############");
+  //     console.error(error);
+  //   }),
+  // ],
 });
 
 // Create a client for your router
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
-
-// Create a client
-const queryClient = new QueryClient();
+import { queryClient } from "./lib/orpc.ts";
 
 createRoot(document.querySelector("#root")!).render(
   <StrictMode>
