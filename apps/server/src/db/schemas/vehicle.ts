2a3c1260d470b769f7aa72b0144ec2e6b64b46fb
François Lachèse <francois.lachese@gmail.com>
2025-10-08 18:34:44 +0200

    bd: add fields to vehicle schema

--- apps/server/src/db/schemas/vehicle.ts
+++ apps/server/src/db/schemas/vehicle.ts
@@ -1,10 +1,28 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { createSchemaFactory, createSelectSchema } from "drizzle-zod";
 
 import { timestamps } from "../helpers/timestamps";
 
 export const vehiclesTable = sqliteTable("vehicle", {
   id: integer().primaryKey({ autoIncrement: true }),
-  name: text().notNull(),
+  brand: text().notNull(),
   description: text(),
+  engine: text(),
+  model: text().notNull(),
+  power: integer(),
+  trim: text(),
+  year: integer(),
   ...timestamps,
+});
+
+const { createInsertSchema } = createSchemaFactory({
+  coerce: {
+    number: true,
+  },
+});
+
+export const vehicleSchema = createSelectSchema(vehiclesTable);
+export const vehicleInsertSchema = createInsertSchema(vehiclesTable, {
+  brand: (schema) => schema.min(1),
+  model: (schema) => schema.min(1),
 });
