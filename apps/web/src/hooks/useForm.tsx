ab294008581dd1e9171dfd5b65c918fb47ed151f
François Lachèse <francois.lachese@gmail.com>
2025-10-09 10:14:06 +0200

    fix: remove weird lazy loading

--- apps/web/src/hooks/useForm.tsx
+++ apps/web/src/hooks/useForm.tsx
@@ -1,11 +1,9 @@
 import { createFormHook } from "@tanstack/react-form";
-import { lazy } from "react";
 
 import { SubscribeButton } from "../components/ui/form/Button";
 import NumberField from "../components/ui/form/NumberField";
+import TextField from "../components/ui/form/TextField";
 import { fieldContext, formContext } from "../contexts/form-context";
-
-const TextField = lazy(() => import("../components/ui/form/TextField"));
 
 export const { useAppForm, withFieldGroup, withForm } = createFormHook({
   fieldContext,
