2d58be728be7d721bd4ba2be9e4175a2e9620a54
François Lachèse <francois.lachese@gmail.com>
2025-10-16 14:37:45 +0200

    feat(web): add queryclient to router context

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -10,7 +10,17 @@ import { queryClient } from "./lib/orpc.ts";
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreload: "intent",
+  defaultPreloadStaleTime: 0,
+  routeTree,
+  scrollRestoration: true,
+  context: {
+    queryClient,
+  },
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
