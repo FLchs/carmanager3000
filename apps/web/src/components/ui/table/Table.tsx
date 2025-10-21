148e9e119a3ae02ea7c06c9dedaf798925c33d85
François Lachèse <francois.lachese@gmail.com>
2025-10-21 19:00:01 +0200

    feat(web/ui): Add table component

---
+++ apps/web/src/components/ui/table/Table.tsx
@@ -0,0 +1,39 @@
+type Props = {
+  headers: string[];
+  rows: Array<Record<string, number | string> & { id: number | string }>;
+};
+
+export const Table = ({ headers, rows }: Props) => {
+  return (
+    <table className="w-full text-left">
+      <thead className="font-bold border-b border-border">
+        <tr>
+          {headers.map((header) => (
+            <th className="px-4 py-2" key={`header-${header}`} scope="col">
+              {header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row) => {
+          const { id, ...data } = row;
+          return (
+            <tr
+              className="border-b  border-border hover:bg-bg-dark last:border-0"
+              key={id}
+            >
+              {Object.entries(data).map(([key, value]) => {
+                return (
+                  <td className="py-2 px-4" key={key}>
+                    {value}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
