46ba77a3f29f7397b7e8c07634491473427cd644
François Lachèse <francois.lachese@gmail.com>
2025-10-25 19:00:07 +0200

    wip: stop using drizzle-zod

--- apps/server/src/db/schemas/validation.ts
+++ apps/server/src/db/schemas/validation.ts
@@ -6,22 +6,36 @@ import { vehiclesTable } from "./vehicle";
 
 const { createInsertSchema, createSelectSchema } = createSchemaFactory({
   coerce: {
-    number: true,
+    // date: true,
+    // number: true,
   },
 });
 
 export const maintenanceLogSelectSchema =
   createSelectSchema(maintenanceLogTable);
 
-export const maintenanceLogInsertSchema = createInsertSchema(
-  maintenanceLogTable,
-  {},
-);
+export const maintenanceLogInsertSchema =
+  createInsertSchema(maintenanceLogTable);
+
+export const mlInsertSchema = z.object({
+  date: z.coerce.date<Date>(),
+  mileage: z.coerce.number<number>(),
+  note: z.string(),
+  type: z.string(),
+});
 
 export const vehicleSchema = createSelectSchema(vehiclesTable);
-export const vehicleWithLogSchema = createSelectSchema(vehiclesTable).extend({
-  maintenanceLog: z.array(maintenanceLogSelectSchema),
-});
+export const vehicleWithLogSchema = createSelectSchema(vehiclesTable)
+  .extend({
+    maintenanceLog: z.array(
+      maintenanceLogSelectSchema.omit({
+        createdAt: true,
+        updatedAt: true,
+        vehicleId: true,
+      }),
+    ),
+  })
+  .omit({ createdAt: true, updatedAt: true });
 export const vehicleInsertSchema = createInsertSchema(vehiclesTable, {
   brand: (schema) => schema.min(1),
   model: (schema) => schema.min(1),
