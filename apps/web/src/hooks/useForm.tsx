b4f1d0172ccd8954c9dc4a3719a0b30a246e237e
François Lachèse <francois.lachese@gmail.com>
2025-10-25 19:02:14 +0200

    chore: do not forger to export field

--- apps/web/src/hooks/useForm.tsx
+++ apps/web/src/hooks/useForm.tsx
@@ -1,5 +1,7 @@
 import { createFormHook } from "@tanstack/react-form";
 
+import DateField from "@/components/ui/form/DateField";
+
 import { SubscribeButton } from "../components/ui/form/Button";
 import NumberField from "../components/ui/form/NumberField";
 import TextField from "../components/ui/form/TextField";
@@ -9,6 +11,7 @@ export const { useAppForm, withFieldGroup, withForm } = createFormHook({
   fieldContext,
   formContext,
   fieldComponents: {
+    DateField,
     NumberField,
     TextField,
   },
