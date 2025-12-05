27261365ee8ba613e506e4e65e6745086ebe6c22
François Lachèse <francois.lachese@gmail.com>
2025-12-05 16:17:45 +0100

    feat: migrate whole workspace from eslint to oxc

--- apps/web/eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import { reactConfig } from "@cm3k/eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-
-export default defineConfig([
-  globalIgnores(["src/routeTree.gen.ts"]),
-  ...reactConfig(),
-]);
