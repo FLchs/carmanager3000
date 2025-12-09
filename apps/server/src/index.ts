268d86882b2638352f8edcaa662efeb4aa3820e4
François Lachèse <francois.lachese@gmail.com>
2025-12-09 08:37:16 +0100

    feat: add production logs

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,8 +3,11 @@ import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
 import { type RouterClient } from "@orpc/server";
 import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
 import { Hono } from "hono";
+import { pinoLogger } from "hono-pino";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
+import { requestId } from "hono/request-id";
+import pino from "pino";
 
 import { migrateDb } from "./db";
 import { router } from "./routers";
@@ -14,21 +17,28 @@ migrateDb();
 
 const app = new Hono();
 
+app.use(requestId());
+
 export const apiHandler = new OpenAPIHandler(router, {
   plugins: [
     new OpenAPIReferencePlugin({
-      schemaConverters: [
-        new ZodToJsonSchemaConverter(),
-        // new experimental_ArkTypeToJsonSchemaConverter(),
-        // new experimental_ArkTypeToJsonSchemaConverter({
-        //   fallback: (ctx) => ctx.base,
-        // }),
-      ],
+      schemaConverters: [new ZodToJsonSchemaConverter()],
     }),
   ],
 });
 
-app.use(logger());
+app.use(
+  process.env.NODE_ENV === "PROD"
+    ? pinoLogger({
+        pino: {
+          level: "debug",
+          transport: { target: "hono-pino/debug-log", options: { colorEnabled: true } },
+          timestamp: pino.stdTimeFunctions.unixTime,
+        },
+      })
+    : logger(),
+);
+
 app.use(
   "/*",
   cors({
