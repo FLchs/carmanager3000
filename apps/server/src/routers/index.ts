87084c6db04d2f0b67b0db46b490faebda56cdb6
François Lachèse <francois.lachese@gmail.com>
2026-01-07 14:58:21 +0100

    fix: types

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,10 +1,9 @@
-import { errorMiddleWare } from "#middlewares/errorMiddleWare";
 import { os } from "@orpc/server";
 
 import { operationsRouter } from "./operations";
 import { vehiclesRouter } from "./vehicles";
 
-export const router = os.use(errorMiddleWare).router({
+export const router = os.router({
   operations: operationsRouter,
   vehicles: vehiclesRouter,
 });
--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -1,3 +1,2 @@
 export { vehiclesContract } from "#contracts/vehicles";
 export { errors } from "#errors";
-export { NotFoundError, ServiceError, DbError } from "#lib/errors";
