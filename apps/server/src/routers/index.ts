fffa992bfeb41b95b1f29e8759701739f3a986bb
François Lachèse <francois.lachese@gmail.com>
2026-01-08 15:26:51 +0100

    fix(server): clean code

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -1,21 +1,10 @@
-import { NotFoundError } from "#lib/errors";
 import { errorMiddleware } from "#middlewares/errorMiddleware";
-import { onError, ORPCError, os } from "@orpc/server";
+import { os } from "@orpc/server";
 
 import { operationsRouter } from "./operations";
 import { vehiclesRouter } from "./vehicles";
 
-export const router = os
-  // .use(
-  //   onError((err) => {
-  //     console.log(err);
-  //     if (err instanceof NotFoundError) {
-  //       throw new ORPCError("TOO_MANY_REQUESTS");
-  //     }
-  //   }),
-  // )
-  .use(errorMiddleware)
-  .router({
-    operations: operationsRouter,
-    vehicles: vehiclesRouter,
-  });
+export const router = os.use(errorMiddleware).router({
+  operations: operationsRouter,
+  vehicles: vehiclesRouter,
+});
