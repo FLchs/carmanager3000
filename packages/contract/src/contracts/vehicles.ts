0506b9086a8b4723313f678d05a89acc87d1fda2
François Lachèse <francois.lachese@gmail.com>
2026-01-20 19:44:29 +0100

    feat(contracts): add documents routes

--- packages/contract/src/contracts/vehicles.ts
+++ packages/contract/src/contracts/vehicles.ts
@@ -7,6 +7,9 @@ import {
   createVehicleSchema,
   listVehiclesSchema,
   getOperationSchema,
+  listDocumentsSchema,
+  createDocumentSchema,
+  getDocumentSchema,
 } from "@cm3k/validation";
 import { oc } from "@orpc/contract";
 import { z } from "zod/v4";
@@ -94,6 +97,39 @@ const operations = {
     .output(successSchema),
 };
 
+const documents = {
+  create: oc
+    .route({
+      method: "POST",
+      path: "/{vehicleId}/documents",
+      inputStructure: "detailed",
+    })
+    .input(
+      z.object({
+        body: createDocumentSchema,
+        params: z.object({ vehicleId: z.coerce.number<number>() }),
+      }),
+    )
+    .output(getDocumentSchema),
+
+  list: oc
+    .route({
+      inputStructure: "detailed",
+      method: "GET",
+      path: "/{vehicleId}/documents",
+    })
+    .input(z.object({ params: z.object({ vehicleId: z.coerce.number<number>() }) }))
+    .output(listDocumentsSchema),
+
+  remove: oc
+    .route({
+      method: "DELETE",
+      path: "/{vehicleId}/{id}",
+    })
+    .input(z.object({ id: z.coerce.number<number>() }))
+    .output(successSchema),
+};
+
 export const vehiclesContract = oc.errors(errors).prefix("/vehicles").router({
   vehicles: {
     update,
@@ -102,5 +138,6 @@ export const vehiclesContract = oc.errors(errors).prefix("/vehicles").router({
     get,
     remove,
     operations,
+    documents,
   },
 });
