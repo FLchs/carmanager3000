17db9f9968acf880a2033c2ef32ff945c09bf76e
François Lachèse <francois.lachese@gmail.com>
2026-01-21 17:07:48 +0100

    feat(web): use new contract

--- apps/web/src/lib/openapi.ts
+++ apps/web/src/lib/openapi.ts
@@ -1,11 +1,13 @@
 import type { ContractRouterClient } from "@orpc/contract";
 import type { JsonifiedClient } from "@orpc/openapi-client";
 
-import { vehiclesContract as contract } from "@cm3k/contract";
+import { vehiclesContract, documentsContract } from "@cm3k/contract";
 import { createORPCClient } from "@orpc/client";
 import { OpenAPILink } from "@orpc/openapi-client/fetch";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
+
+const contract = { ...vehiclesContract, ...documentsContract };
 
 const link = new OpenAPILink(contract, {
   url: "http://localhost:3000/api",
