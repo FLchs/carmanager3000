4984ecdbf7a0804993bc7adb2bb69176ac7d07dd
François Lachèse <francois.lachese@gmail.com>
2026-01-26 16:49:24 +0100

    feat(web): add nice looking custom number field

--- apps/web/src/components/ui/form/NumberField.tsx
+++ apps/web/src/components/ui/form/NumberField.tsx
@@ -1,31 +1,71 @@
 import { useStore } from "@tanstack/react-form";
+import { MinusIcon, PlusIcon, X } from "lucide-react";
+import { useCallback } from "react";
 
 import { useFieldContext } from "../../../contexts/form-context";
 import FormErrors from "./FormErrors";
 
-export default function NumberField({ label }: { label: string }) {
-  const field = useFieldContext<number>();
+export default function NumberField({
+  label,
+  nullable = false,
+}: {
+  label: string;
+  nullable?: boolean;
+}) {
+  const field = useFieldContext<number | "">();
+
+  const updateValue = useCallback(
+    (int: number) => {
+      field.handleChange(Math.max(Number(field.state.value) + int, 0));
+    },
+    [field],
+  );
 
   const errors = useStore(field.store, (state) => state.meta.errors);
 
   return (
-    <div className="grid">
+    <div>
       <label className="text-text-muted" htmlFor={field.name}>
-        {label}
+        {label}:
       </label>
-      <input
-        className="bg-bg-light border-border text-text-muted rounded-lg border p-2 outline-0"
-        id={field.name}
-        name={field.name}
-        value={field.state.value}
-        type="number"
-        inputMode="numeric"
-        onChange={(e) => {
-          const val = e.target.valueAsNumber;
-          field.handleChange(Number.isNaN(val) ? 0 : val);
-        }}
-      />
-      <FormErrors errors={errors} />
+      <div className="flex flex-row bg-bg-light rounded-lg w-fit mt-1">
+        <button
+          type="button"
+          className="border-r-2 border-bg-dark p-1 cursor-pointer"
+          onClick={() => updateValue(-1)}
+        >
+          <MinusIcon className="w-6 m-auto px-1" />
+        </button>
+        <div className="relative px-1">
+          <input
+            size={6}
+            className="text-text-muted h-full pr-6 pl-2 outline-0"
+            id={field.name}
+            name={field.name}
+            value={field.state.value}
+            onChange={(e) => {
+              const val = parseInt(e.target.value);
+              field.handleChange(Number.isNaN(val) ? 0 : val);
+            }}
+          />
+          <button className="absolute right-1.5 h-full" type="button">
+            <X
+              className="w-4.5 cursor-pointer rounded-full h-4.5 p-0.5 text-text-muted"
+              onClick={() => field.clearValues()}
+            />
+          </button>
+        </div>
+        {nullable && (
+          <button
+            type="button"
+            className="border-l-2 border-bg-dark p-1 cursor-pointer"
+            onClick={() => updateValue(1)}
+          >
+            <PlusIcon className="w-6 m-auto px-1" />
+          </button>
+        )}
+        <FormErrors errors={errors} />
+      </div>
     </div>
   );
 }
