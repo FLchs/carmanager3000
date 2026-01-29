c64e06b721a56e43244bcd9fb7ccf34128bd3335
François Lachèse <francois.lachese@gmail.com>
2026-01-29 18:19:02 +0100

    feat(web): add new fields to useForm hook

--- apps/web/src/hooks/useForm.tsx
+++ apps/web/src/hooks/useForm.tsx
@@ -1,21 +1,25 @@
 import { createFormHook } from "@tanstack/react-form";
 
+import CheckboxField from "@/components/ui/form/CheckboxField";
 import DateField from "@/components/ui/form/DateField";
 import FileField from "@/components/ui/form/FileField";
+import NumberField from "@/components/ui/form/NumberField";
+import SelectField from "@/components/ui/form/SelectField";
+import TextField from "@/components/ui/form/TextField";
 
 import { SubscribeButton } from "../components/ui/form/Button";
-import NumberField from "../components/ui/form/NumberField";
-import TextField from "../components/ui/form/TextField";
 import { fieldContext, formContext } from "../contexts/form-context";
 
 export const { useAppForm, withFieldGroup, withForm } = createFormHook({
   fieldContext,
   formContext,
   fieldComponents: {
-    DateField,
     NumberField,
     TextField,
     FileField,
+    SelectField,
+    CheckboxField,
+    DateField,
   },
   formComponents: {
     SubscribeButton,
