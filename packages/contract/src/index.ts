3abc28918780051a7520cd6f36c0f1bd0ef1e147
Francois Lachese <francois.lachese@gmail.com>
2026-02-07 14:08:46 +0100

    feat(contract): add operations list contract

--- packages/contract/src/index.ts
+++ packages/contract/src/index.ts
@@ -1,4 +1,17 @@
+import { documentsContract } from "#contracts/documents";
+import { documentTypesContract } from "#contracts/documentTypes";
+import { operationsContract } from "#contracts/operations";
+import { vehiclesContract } from "#contracts/vehicles";
+
 export { vehiclesContract } from "#contracts/vehicles";
 export { documentsContract } from "#contracts/documents";
 export { documentTypesContract } from "#contracts/documentTypes";
+export { operationsContract } from "#contracts/operations";
 export { errors } from "#errors";
+
+export const contract = {
+  ...vehiclesContract,
+  ...documentsContract,
+  ...documentTypesContract,
+  ...operationsContract,
+};
