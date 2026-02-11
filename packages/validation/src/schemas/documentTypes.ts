56b3f4e62388ab24f5ed8afaeafc244495b3ffd8
François Lachèse <francois.lachese@gmail.com>
2026-02-11 10:04:30 +0100

    feat(validation): add document type creation schema

--- packages/validation/src/schemas/documentTypes.ts
+++ packages/validation/src/schemas/documentTypes.ts
@@ -1,8 +1,17 @@
 import * as z from "zod/v4";
 
+export const documentTypeInSchema = z.object({
+  name: z.string(),
+  slug: z.string(),
+});
+
 export const documentTypeOutSchema = z.object({
   id: z.number().int(),
   name: z.string(),
 });
 
 export const listDocumentTypesSchema = z.array(documentTypeOutSchema);
+
+export const createDocumentTypeSchema = documentTypeInSchema;
+
+export const getDocumentTypeSchema = documentTypeOutSchema;
