a1c38a327911905a2f5d5a94074417c6f42e85fb
François Lachèse <francois.lachese@gmail.com>
2026-02-12 19:40:52 +0100

    feat(validation): add regex to documentTypes validation schema

--- packages/validation/src/schemas/documentTypes.ts
+++ packages/validation/src/schemas/documentTypes.ts
@@ -2,7 +2,13 @@ import * as z from "zod/v4";
 
 export const documentTypeInSchema = z.object({
   name: z.string(),
-  slug: z.string(),
+  slug: z
+    .string()
+    .min(3)
+    .regex(/^[a-z0-9]+(?:_[a-z0-9]+)*$/, {
+      message:
+        "Slug must contain only lowercase letters, numbers, and single underscores between words",
+    }),
 });
 
 export const documentTypeOutSchema = z.object({
