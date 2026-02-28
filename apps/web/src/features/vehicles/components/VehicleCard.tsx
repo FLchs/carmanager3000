6cf010f20c34f9fc1087831704610aba5bc009da
François Lachèse <francois.lachese@gmail.com>
2026-02-28 15:18:52 +0100

    fix(web): Vehicle list width

--- apps/web/src/features/vehicles/components/VehicleCard.tsx
+++ apps/web/src/features/vehicles/components/VehicleCard.tsx
@@ -12,26 +12,28 @@ function VehicleCard({ vehicle }: { vehicle: Vehicle }) {
   const { id, brand, engine, model, power, trim, year } = vehicle;
 
   return (
-    <Card>
-      <div>
-        <img className="absolute h-12" src="/kia-logo.png" />
-        <Link params={{ vehicleId: id.toString() }} to="/vehicles/$vehicleId">
-          <img className="w-full" src="/kia-magentis.jpg" />
-        </Link>
-      </div>
-      <div className="mt-4 flex justify-between gap-4">
+    <div className="max-w-150">
+      <Card className="w-72">
         <div>
-          <h3 className="text-text mb-2 font-bold">{model}</h3>
-          <p className="text-text-muted">
-            {brand} {model} {trim} - {year}
-          </p>
-          <p className="text-text-muted">
-            {engine} - {power}
-          </p>
+          <img className="absolute h-12" src="/kia-logo.png" />
+          <Link params={{ vehicleId: id.toString() }} to="/vehicles/$vehicleId">
+            <img src="/kia-magentis.jpg" />
+          </Link>
         </div>
-        <VehicleMenu id={id} />
-      </div>
-    </Card>
+        <div className="mt-4 flex justify-between gap-4">
+          <div>
+            <h3 className="mb-2 font-bold text-text">{model}</h3>
+            <p className="text-text-muted">
+              {brand} {model} {trim} - {year}
+            </p>
+            <p className="text-text-muted">
+              {engine} - {power}
+            </p>
+          </div>
+          <VehicleMenu id={id} />
+        </div>
+      </Card>
+    </div>
   );
 }
 
